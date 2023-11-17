9a89c04386b7075659a8db794898749b34c3d995
aryelzx <arielcearamor102@gmail.com>
2023-11-17 18:35:34 -0300

    first commit

---
+++ aryelfilmes/src/App.tsx
@@ -0,0 +1,13 @@
+import { Aside } from "./components/Aside"
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full bg-black">
+        <Aside />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ aryelfilmes/src/components/Aside/index.tsx
@@ -0,0 +1,10 @@
+
+function Aside() {
+  return (
+    <div className="text-red-500 bg-black w-36">
+      teste
+    </div>
+  )
+}
+
+export { Aside }
