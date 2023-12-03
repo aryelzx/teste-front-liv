041e19587c1d9f2dc3ef26b62bd034a078de795d
aryelzx <arielcearamor102@gmail.com>
2023-12-03 12:53:48 -0300

    pass api token to env

--- aryelfilmes/src/shared/api/http.ts
+++ aryelfilmes/src/shared/api/http.ts
@@ -5,7 +5,7 @@ function httpClientBuilder() {
   const client = axios.create({ baseURL })
 
   client.interceptors.request.use((config) => {
-    const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjk0MmRlNjU4MjhmM2E4ZDFiZDE5MGM0ZDU1YWVlYiIsInN1YiI6IjYzNWFiNTdjZDFhODkzMDA3ZDVmMDQ0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7fZlM8KPoUHoYZo6jrtidjIIcoyCyL7ZjzcDjAAiEO8";
+    const token = "apitoken";
     config.headers["Authorization"] = `Bearer ${token}`;
     return config;
   });
